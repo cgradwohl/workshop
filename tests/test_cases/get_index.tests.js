1fc728e27beec0e258d030e8512c20c9a3d59c75
Chris Gradwohl <christophergradwohl@gmail.com>
2020-09-06 12:17:07 -0700

    nnotes.

--- tests/test_cases/get_index.tests.js
+++ tests/test_cases/get_index.tests.js
@@ -1,39 +1,39 @@
-// /**
-//  * Here we have a single test case that will get the response from get-index, inspect its status code,
-//  * Content-Type header and the HTML content to make sure it did return 8 restaurants. The magic, however,
-//  * is in when.we_invoke_get_index, beause it's abstracted away and doesn't specify HOW we invoke 
-//  * get-index, it allows us to reuse this test case later as an acceptance test where we'll invoke
-//  * get-index by calling the deployed HTTP GET / endpoint.
-//  */
-// const cheerio = require('cheerio')
-// const when = require('../steps/when')
-// const { init } = require('../steps/init')
-// console.log = jest.fn() // silence the funtions console logs
+/**
+ * Here we have a single test case that will get the response from get-index, inspect its status code,
+ * Content-Type header and the HTML content to make sure it did return 8 restaurants. The magic, however,
+ * is in when.we_invoke_get_index, beause it's abstracted away and doesn't specify HOW we invoke 
+ * get-index, it allows us to reuse this test case later as an acceptance test where we'll invoke
+ * get-index by calling the deployed HTTP GET / endpoint.
+ */
+const cheerio = require('cheerio')
+const when = require('../steps/when')
+const { init } = require('../steps/init')
+console.log = jest.fn() // silence the funtions console logs
 
-// describe(`When we invoke the GET / endpoint`, () => {
-//   beforeAll(async () => await init())
+describe(`When we invoke the GET / endpoint`, () => {
+  beforeAll(async () => await init())
   
-//   /**
-//    * How can we make the tests more reliable/ resiliant without relying on the data from the database?
-//    * 
-//    * 1. The data that the tests need to assert on inorder to pass,
-//    * should be created during the beforeAll() step. i.e. insert some data
-//    * into the DB.
-//    * 
-//    * 2. The data should then be removed during the afterAll() step.
-//    * 
-//    * 3. Since we are using infrastructure ass code is it considered best practice to spin up
-//    * a new testing stack, setup the data and run the tests against the test stack, then tear it all down.
-//    */
-//   it(`Should return the index page with 8 restaurants`, async () => {
-//     const res = await when.we_invoke_get_index()
+  /**
+   * How can we make the tests more reliable/ resiliant without relying on the data from the database?
+   * 
+   * 1. The data that the tests need to assert on inorder to pass,
+   * should be created during the beforeAll() step. i.e. insert some data
+   * into the DB.
+   * 
+   * 2. The data should then be removed during the afterAll() step.
+   * 
+   * 3. Since we are using infrastructure ass code is it considered best practice to spin up
+   * a new testing stack, setup the data and run the tests against the test stack, then tear it all down.
+   */
+  it(`Should return the index page with 8 restaurants`, async () => {
+    const res = await when.we_invoke_get_index()
 
-//     expect(res.statusCode).toEqual(200)
-//     expect(res.headers['Content-Type']).toEqual('text/html; charset=UTF-8')
-//     expect(res.body).toBeDefined()
+    expect(res.statusCode).toEqual(200)
+    expect(res.headers['Content-Type']).toEqual('text/html; charset=UTF-8')
+    expect(res.body).toBeDefined()
 
-//     const $ = cheerio.load(res.body)
-//     const restaurants = $('.restaurant', '#restaurantsUl')
-//     expect(restaurants.length).toEqual(8)
-//   })
-// });
+    const $ = cheerio.load(res.body)
+    const restaurants = $('.restaurant', '#restaurantsUl')
+    expect(restaurants.length).toEqual(8)
+  })
+});
--- tests/test_cases/place-order.tests.js
+++ tests/test_cases/place-order.tests.js
@@ -1,62 +1,62 @@
-// const when = require('../steps/when')
-// const given = require('../steps/given')
-// const tearDown = require('../steps/tearDown')
-// const { init } = require('../steps/init')
-// const AWS = require('aws-sdk')
-// console.log = jest.fn()
+const when = require('../steps/when')
+const given = require('../steps/given')
+const tearDown = require('../steps/tearDown')
+const { init } = require('../steps/init')
+const AWS = require('aws-sdk')
+console.log = jest.fn()
 
-// const mockPutEvents = jest.fn()
-// // here we are mocking the EventBridge putEvent
-// // The problem is that, to validate the events that are sent to EventBridge it'll
-// // take a bit of extra infrastructure set up. Because you can't just call EventBridge
-// // and ask what events it had just received on a bus recently.
-// // TODO: implement this: https://theburningmonk.com/2019/09/how-to-include-sns-and-kinesis-in-your-e2e-tests/
-// AWS.EventBridge.prototype.putEvents = mockPutEvents
+const mockPutEvents = jest.fn()
+// here we are mocking the EventBridge putEvent
+// The problem is that, to validate the events that are sent to EventBridge it'll
+// take a bit of extra infrastructure set up. Because you can't just call EventBridge
+// and ask what events it had just received on a bus recently.
+// TODO: implement this: https://theburningmonk.com/2019/09/how-to-include-sns-and-kinesis-in-your-e2e-tests/
+AWS.EventBridge.prototype.putEvents = mockPutEvents
 
-// describe('Given an authenticated user', () => {
-//   let user
+describe('Given an authenticated user', () => {
+  let user
 
-//   beforeAll(async () => {
-//     await init()
-//     user = await given.an_authenticated_user()
-//   })
+  beforeAll(async () => {
+    await init()
+    user = await given.an_authenticated_user()
+  })
 
-//   afterAll(async () => {
-//     await tearDown.an_authenticated_user(user)
-//   })
+  afterAll(async () => {
+    await tearDown.an_authenticated_user(user)
+  })
 
-//   describe(`When we invoke the POST /orders endpoint`, () => {
-//     let resp
+  describe(`When we invoke the POST /orders endpoint`, () => {
+    let resp
 
-//     beforeAll(async () => {
-//       mockPutEvents.mockClear()
-//       mockPutEvents.mockReturnValue({
-//         promise: async () => {}
-//       })
+    beforeAll(async () => {
+      mockPutEvents.mockClear()
+      mockPutEvents.mockReturnValue({
+        promise: async () => {}
+      })
 
-//       resp = await when.we_invoke_place_order(user, 'Fangtasia')
-//     })
+      resp = await when.we_invoke_place_order(user, 'Fangtasia')
+    })
 
-//     it(`Should return 200`, async () => {
-//       expect(resp.statusCode).toEqual(200)
-//     })
+    it(`Should return 200`, async () => {
+      expect(resp.statusCode).toEqual(200)
+    })
 
-//     it(`Should publish a message to EventBridge bus`, async () => {
-//       // here we are just validating the event shape that comes out of the event bus put event
-//       // only validate EventBridge was called when executing as an integration test using mocks
-//       // TODO: naming TEST_MODE as 'handler' for integration test mode and 'http' as e2e test mode is really stupid and confusing...change that! :)
-//       if (process.env.TEST_MODE === 'handler') {
-//         expect(mockPutEvents).toBeCalledWith({
-//           Entries: [
-//             expect.objectContaining({
-//               Source: 'big-mouth',
-//               DetailType: 'order_placed',
-//               Detail: expect.stringContaining(`"restaurantName":"Fangtasia"`),
-//               EventBusName: expect.stringMatching(process.env.bus_name)
-//             })
-//           ]
-//         })
-//       }
-//     })
-//   })
-// })
+    it(`Should publish a message to EventBridge bus`, async () => {
+      // here we are just validating the event shape that comes out of the event bus put event
+      // only validate EventBridge was called when executing as an integration test using mocks
+      // TODO: naming TEST_MODE as 'handler' for integration test mode and 'http' as e2e test mode is really stupid and confusing...change that! :)
+      if (process.env.TEST_MODE === 'handler') {
+        expect(mockPutEvents).toBeCalledWith({
+          Entries: [
+            expect.objectContaining({
+              Source: 'big-mouth',
+              DetailType: 'order_placed',
+              Detail: expect.stringContaining(`"restaurantName":"Fangtasia"`),
+              EventBusName: expect.stringMatching(process.env.bus_name)
+            })
+          ]
+        })
+      }
+    })
+  })
+})
